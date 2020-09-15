eceaeff051001e6ddd481b9dcfa901b89ba32fd7
Thao <nguyenthothao1996@gmail.com>
2020-09-15 17:38:54 +0700

    product details

--- src/Screen/ProductScreen.js
+++ src/Screen/ProductScreen.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import data from '../data';
+import { Link } from 'react-router-dom';
 
 function ProductScreen(props){
     const product = data.products.find(x=> x._id === parseInt(props.match.params.id,10));
     console.log(product);
     return (
         <div>
-        <h2>{product.name}</h2>
-        <img src={product.image} alt="product"/>
+            <div className="back-homepage"><Link to="/">Back to homepage</Link></div>
+            <div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt="product"/>
+                </div>
+                <div className="details-info">
+                    <ul>
+                        <li className="product-name">{product.name}</li>
+                        <li className="product-price">Price: ${product.price}</li>
+                        <li><p className="product-price">Description:</p> {product.description}</li>
+                    </ul>
+                </div>
+                <div className="details-action">
+                    <ul>
+                        <li>Price : ${product.price}</li>
+                        <li>Status : {product.status}</li>
+                        <li>Quantity: <select>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                </select>
+                        </li>
+                        <li>
+                            <button className="button">Add to cart</button>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/data.js
+++ src/data.js
@@ -6,7 +6,8 @@ export default {
             image: "/images/2.jpg",
             brand: "ROLEX",
             price: 59,
-            rating: 4
+            rating: 4,
+            description:"A silvertone dial and screw-head design are featured on this Nemesis watch. This fine timepiece secures with a brown leather strap."
         },
         {
             _id: 2,
@@ -14,7 +15,8 @@ export default {
             image: "/images/3.jpg",
             brand: "APPLE",
             price: 99,
-            rating: 5
+            rating: 5,
+            description:"This classic timepiece by Burberry features a stainless steel case and haymarket leather strap. A champagne with hydraulic-stamped check pattern dial, precise quartz movement and a water-resistance level of up to 50 meters."
         },
         {
             _id: 3,
@@ -22,7 +24,8 @@ export default {
             image: "/images/4.jpg",
             brand: "Burgi",
             price: 199,
-            rating: 4.8
+            rating: 4.8,
+            description:"This charming Burgi women's watch is powered by Japanese quartz movement. It features a mother of pearl dial and sparkling diamond markers. The long sleek hands and dazzling crystals make this timepiece elegant and unique."
         },
         {
             _id: 4,
@@ -30,15 +33,17 @@ export default {
             image: "/images/2.jpg",
             brand: "LUXURY",
             price: 79,
-            rating: 3.7
+            rating: 3.7,
+            description:"Performance and style are combined for magnificent flair in this men's Ascot watch from Stuhrling Original. The watch features rhythmic Swiss quartz movement for reliable, high-precision time-keeping, and the leather strap is alligator embossed for rugged flair. The finishing ensures a timeless effect, which is emphasized by the powerful contrast. To help you find exactly what you are looking for in a watch, several color options are available."
         },
         {
             _id: 5,
-            name: "Night Watch",
-            image: "/images/2.jpg",
-            brand: "ROLEX",
+            name: "Seamaster Watch",
+            image: "/images/5.jpg",
+            brand: "Omega Men's",
             price: 60,
-            rating: 4
+            rating: 4,
+            description:"Stainless steel case, Stainless steel bracelet, Silver dial, Quartz movement, Scratch resistant sapphire, Water resistant up to 15 ATM - 150 meters -500 feet"
         },
         {
             _id: 6,
@@ -46,7 +51,8 @@ export default {
             image: "/images/2.jpg",
             brand: "ROLEX",
             price: 60,
-            rating: 4
+            rating: 4,
+            description:"This classic timepiece by Burberry features a stainless steel case and haymarket leather strap. A champagne with hydraulic-stamped check pattern dial, precise quartz movement and a water-resistance level of up to 50 meters."
         }
     ]
 };
